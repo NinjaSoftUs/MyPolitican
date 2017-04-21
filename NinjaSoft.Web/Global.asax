04b10f77f2e1f42661591ecb1023ca9777412259
David Brighton <DaveBrighton@msn.com>
2017-04-20 20:15:56 -0600

    Add project files.

---
+++ NinjaSoft.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NinjaSoft.Web.MvcApplication" Language="C#" %>
---
+++ NinjaSoft.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NinjaSoft.Web.Startup))]
+namespace NinjaSoft.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
